52f8f0e93cc9e40da6db3fd8973a36689cd082d6
qwordy <iamyfy@163.com>
2019-05-15 00:32:14 +0800

    grpc demo

---
+++ src/main/java/com/jraft/HelloWorldClient.java
@@ -0,0 +1,60 @@
+package com.jraft;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+
+import java.util.concurrent.TimeUnit;
+
+public class HelloWorldClient {
+
+    private final ManagedChannel channel;
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client connecting to HelloWorld server at {@code host:port}. */
+    public HelloWorldClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build());
+    }
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    private HelloWorldClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+//        logger.info("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+//            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        System.out.println(response.getMessage());
+//        logger.info("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        try {
+            client.greet("hehe");
+        } finally {
+            client.shutdown();
+        }
+    }
+}
---
+++ src/main/java/com/jraft/HelloWorldServer.java
@@ -0,0 +1,61 @@
+package com.jraft;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+public class HelloWorldServer {
+
+    private Server server;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+//        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                HelloWorldServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    private static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+}
