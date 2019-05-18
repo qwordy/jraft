595ebde07385797de95952b48dee90fcc573c706
qwordy <iamyfy@163.com>
2019-05-18 12:59:36 +0800

    Add rpc interface comment

--- src/main/java/com/jraft/JraftClient.java
+++ src/main/java/com/jraft/JraftClient.java
@@ -22,6 +22,14 @@ public class JraftClient {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
+    /**
+     * Invoked by candidates to gather votes
+     * @param term candidate’s term
+     * @param candidateId candidate requesting vote
+     * @param lastLogIndex index of candidate’s last log entry
+     * @param lastLogTerm term of candidate’s last log entry
+     * @return RequestVoteReply
+     */
     public RequestVoteReply requestVote(int term, String candidateId, int lastLogIndex, int lastLogTerm) {
         RequestVoteRequest request = RequestVoteRequest.newBuilder()
                 .setTerm(term)
@@ -32,6 +40,16 @@ public class JraftClient {
         return blockingStub.requestVote(request);
     }
 
+    /**
+     * Invoked by leader to replicate log entries; also used as heartbeat
+     * @param term leader’s term
+     * @param leaderId so follower can redirect clients
+     * @param prevLogIndex index of log entry immediately preceding new ones
+     * @param prevLogTerm term of prevLogIndex entry
+     * @param entries log entries to store (empty for heartbeat; may send more than one for efficiency)
+     * @param leaderCommit leader’s commitIndex
+     * @return AppendEntriesReply
+     */
     public AppendEntriesReply appendEntries(int term, String leaderId, int prevLogIndex, int prevLogTerm, List<String> entries, int leaderCommit) {
         AppendEntriesRequest request = AppendEntriesRequest.newBuilder()
                 .setTerm(term)
